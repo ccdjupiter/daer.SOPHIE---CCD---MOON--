b227bab52413bef8eaf0473b12fdddc12908cce1
ccdjupiter <ccdjupiter@gmail.com>
2023-03-01 11:15:44 -0600

    Collapsible accordion question-box created with CSS styling properties added. Collapsible accordion question-box has contents of paragraph, and a question.

--- min.js
+++ min.js
@@ -14,3 +14,12 @@ buttons.forEach( button =>{
 document.querySelector('.menu-icon').addEventListener('click', function() {
     document.querySelector('.menu').classList.toggle('active');
   });
+
+  const accordionHeaders = document.querySelectorAll('.accordion-header');
+
+accordionHeaders.forEach(header => {
+  header.addEventListener('click', () => {
+    const accordionItem = header.parentElement;
+    accordionItem.classList.toggle('active');
+  });
+});
