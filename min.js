e7d11f69878bba1eba4e29a31de7cb0f20b503d8
ccdjupiter <ccdjupiter@gmail.com>
2023-03-06 14:01:02 -0600

    Script from font-awesome is added to head of page: Icons on contact page aren't displaying.

--- min.js
+++ min.js
@@ -25,3 +25,55 @@ accordionHeaders.forEach(header => {
 });
 
 
+const form = document.querySelector('form');
+const firstName = form.querySelector('#firstName');
+const email = form.querySelector('#email');
+const number = form.querySelector('#number');
+const message = form.querySelector('#message');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // handle form validation
+  if (firstName.value.trim() === '') {
+    alert('Please enter your first name');
+    return;
+  }
+
+  if (email.value.trim() === '') {
+    alert('Please enter your email');
+    return;
+  }
+
+  if (number.value.trim() === '') {
+    alert('Please enter your phone number');
+    return;
+  }
+
+  if (message.value.trim() === '') {
+    alert('Please enter your message');
+    return;
+  }
+
+  // prepare data to be sent to the server
+  const data = {
+    firstName: firstName.value.trim(),
+    email: email.value.trim(),
+    number: number.value.trim(),
+    message: message.value.trim(),
+  };
+
+  // send form data to the server using AJAX
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', '/sendmessage', true);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.onload = function() {
+    if (this.status === 200) {
+      alert('Message sent successfully');
+      form.reset();
+    } else {
+      alert('Failed to send message');
+    }
+  };
+  xhr.send(JSON.stringify(data));
+});
